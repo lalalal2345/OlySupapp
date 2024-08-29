5512b11dbb1e815799b1cf21f7006c968e47ab57
GermanUrh <geraurh@mail.ru>
2024-08-29 14:05:29 +0300

    Add more time3

--- OlySupapp/MainViewController.swift
+++ OlySupapp/MainViewController.swift
@@ -69,6 +69,53 @@ var wvObject: WKWebView = {
         return view
     }()
     
+    //onboarding views
+    let viewOnboardingObject: UIImageView = {
+        let view = UIImageView()
+        view.isUserInteractionEnabled = true
+        view.image = UIImage(named: "bg1")
+        return view
+    }()
+    
+    let viewOnboardingImageObject: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "title")
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    let textViewOnboarding: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = UIColor(white: 1, alpha: 0.3)
+        textView.textColor = .black
+        textView.font = UIFont(name: "Futura-Medium", size: 20)
+        //textView.font = UIFont.systemFont(ofSize: 20)
+        textView.text = "First level here? This is an exciting arcade game that immerses you in the world of Greek mythology. As a brave hero, you embark on an exciting adventure through several levels, each of which is full of challenges and dangers. Control the character, collecting shiny bonus coins and passing obstacles. On each level you will face sharp cliffs, deep chasms and tricky traps that require dexterity and wit. Be attentive and skilfully dodge aggressive monsters that seek to stop your path to glory. Use your bow and arrows to defeat your enemies. Learn to aim accurately to fight your opponents. With each level the difficulty increases and enemies become more cunning, ready to test your strength and dexterity. Collect coins, open new locations and become a real hero of Olympus! Are you ready to take on the challenge and prove your valour in Olympus Supreme? Immerse yourself in this exciting world and become a master of your craft!"
+        return textView
+    }()
+    
+    let loadingLabel: UILabel = {
+        let loadingLabel = UILabel()
+        loadingLabel.text = "LOADING"
+        loadingLabel.font = UIFont(name: "Futura-Medium", size: 38)
+        loadingLabel.textColor = .black
+        loadingLabel.translatesAutoresizingMaskIntoConstraints = false
+        return loadingLabel
+    }()
+    
+    lazy var playOnboardingImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "Group 114")
+        view.isUserInteractionEnabled = true
+        view.clipsToBounds = true
+        
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(onPlayOnboardingTapped))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
@@ -89,11 +136,40 @@ var wvObject: WKWebView = {
             self?.viewLoadingObject.isHidden = true
             self?.viewLoadingImageObject.isHidden = true
             self?.loadingViewImageLoading.isHidden = true
+            
+            self?.viewOnboardingObject.isHidden = false
+            self?.viewOnboardingImageObject.isHidden = false
+            self?.playOnboardingImageView.isHidden = true // true PLAY
+            self?.textViewOnboarding.isHidden = false
+            self?.loadingLabel.isHidden = false
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+                self?.loadingLabel.text = "LOADING."
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+                self?.loadingLabel.text = "LOADING.."
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [weak self] in
+                self?.loadingLabel.text = "LOADING..."
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 6) { [weak self] in
+                //self?.loadingLabel.isHidden = true
+                self?.playOnboardingImageView.isHidden = false
+            }
         }
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    @objc func onPlayOnboardingTapped() {
+        print("gooooooooood")
+        viewOnboardingObject.isHidden = true
+        viewOnboardingImageObject.isHidden = true
+        playOnboardingImageView.isHidden = true
+        textViewOnboarding.isHidden = true
     }
     
     func addNewSubviews() {
@@ -113,6 +189,12 @@ var wvObject: WKWebView = {
         pulseAnimation.autoreverses = true
         pulseAnimation.repeatCount = .greatestFiniteMagnitude
         self.loadingViewImageLoading.layer.add(pulseAnimation, forKey: nil)
+        
+        view.addSubview(viewOnboardingObject)
+        view.addSubview(viewOnboardingImageObject)
+        view.addSubview(textViewOnboarding)
+        view.addSubview(playOnboardingImageView)
+        
     }
     
     func setKonsaints() {
@@ -154,10 +236,55 @@ var wvObject: WKWebView = {
             make.height.equalTo(86)
             make.width.equalTo(250)
         }
+        
+        //onboardingViews
+        viewOnboardingObject.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
+            make.bottom.equalTo(view.snp.bottom)
+        }
+        
+        viewOnboardingImageObject.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).offset(100)
+            make.centerX.equalTo(view.snp.centerX)
+            make.height.equalTo(158)
+            make.width.equalTo(275)
+        }
+        
+        textViewOnboarding.snp.makeConstraints { make in
+            make.top.equalTo(viewOnboardingImageObject.snp.bottom).offset(60)
+            make.centerX.equalTo(viewOnboardingImageObject.snp.centerX)
+            make.height.equalTo(300)
+            make.leading.equalTo(view.snp.leading).offset(30)
+            make.trailing.equalTo(view.snp.trailing).inset(30)
+        }
+        
+        playOnboardingImageView.snp.makeConstraints { make in
+            make.top.equalTo(textViewOnboarding.snp.bottom).offset(60)
+            make.centerX.equalTo(viewLoadingImageObject.snp.centerX)
+            make.height.equalTo(86)
+            make.width.equalTo(250)
+        }
+        
+        viewOnboardingObject.addSubview(loadingLabel)
+        loadingLabel.snp.makeConstraints { make in
+            make.top.equalTo(playOnboardingImageView.snp.bottom).offset(20)
+            make.centerX.equalTo(viewLoadingImageObject.snp.centerX)
+            make.height.equalTo(86)
+            make.width.equalTo(250)
+        }
+        
+        //make onboarding hidden
+        viewOnboardingObject.isHidden = true
+        viewOnboardingImageObject.isHidden = true
+        playOnboardingImageView.isHidden = true
+        textViewOnboarding.isHidden = true
+        loadingLabel.isHidden = true
     }
     
     @objc func onBackButtonTapped() {
-        if checkDate(customDate: "08/31/2024") { // - mm/dd/yyyy
+        if checkDate(customDate: "09/02/2024") { // - mm/dd/yyyy
             print("")
             navigationController?.popViewController(animated: false)
         } else {
